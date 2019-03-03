8b13e33372c2b66d0ce0819862324525a63850ec
Gabriel V <gabrielvalenzuela12@gmail.com>
2019-03-03 15:57:26 -0300

    Add doxygen!

--- Laboratory_1/documentation.h
+++ Laboratory_1/documentation.h
@@ -1,43 +1,43 @@
 /**
-********************************************************************************
-*       	Laboratory number one: Cration of a ksmap software
-*
-*  @author: Valenzuela Gabriel Emanuel.
-*  @subject: Operating Systems I - 7422.
-*  @professor: CE Pablo, Martinez.
-*  @university: Universidad Nacional de Córdoba - Facultad de Ciencias 
-*  Exactas, Físicas y Naturales.					  
-*  @date: March 2019
-******************************************************************************
-*/
+   \file documentation.h
+   \brief Introduction
+     
+	  Laboratory number one: Cration of a ksmap software
 
- 
-/**
-*  This software takes a serie of arguments from the user and print the 
-*  selected information. Search on the \proc directory and get 
-*  informataion 
-*  about the system. The input parameters work of the following way:
-*
-*  -a 
-*	Print all information avaible by the software
-*
-*  -c
-*	Print only the information refers to the CPU. That is cantity of 
-*	time's use of CPU by users,system and idle process. Amoung of 
-*	context change. Start date of the system. Number of process 
-*	creates from the day 0
-*
-*  -k  
-*	Print the information refers to the kernel. Version kernel, model 
-*	and type of CPU, amoung of time from the day 0, amoung of systems 
-*	suppor by the kernel
-*
-*  -h
-*	Makes the information size readble for humans
-*
-*  -l [interval duration]
-*	Print the number of petitions maked to the disk. Amoung of memory 
-*	configurated on the hardware. Amoung of memory avaible and list 
-*	the mean of load of a minute.
+  \author Valenzuela Gabriel Emanuel.
+  \version 1.0.0
+  \details \b Subject: Operating Systems I - 7422 \n
+	   \b Professor CE Pablo, Martinez \n
+	   <b>Universidad Nacional de Córdoba - Facultad de 
+	  Ciencias Exactas, Físicas y Naturales</b> \n
+  \date March 2019
+  \copyright MIT License
+  
+  \brief Program's functionallity
+  This software takes a serie of arguments from the user and print the
+  selected information. Search on the \proc directory and get 
+  informataion 
+  about the system. The input parameters work of the following way:
+
+  -a 
+	Print all information avaible by the software
+
+  -c
+	Print only the information refers to the CPU. That is cantity of 
+	time's use of CPU by users,system and idle process. Amoung of 
+	context change. Start date of the system. Number of process 
+	creates from the day 0
+
+  -k  
+	Print the information refers to the kernel. Version kernel, model 
+	and type of CPU, amoung of time from the day 0, amoung of systems 
+	suppor by the kernel
+   -h
+	Makes the information size readble for humans
+
+   -l [interval duration]
+	Print the number of petitions maked to the disk. Amoung of memory 
+	configurated on the hardware. Amoung of memory avaible and list 
+	the mean of load of a minute.
 */
 
---
+++ Laboratory_1/html/navtreeindex0.js
@@ -0,0 +1,12 @@
+var NAVTREEINDEX0 =
+{
+"documentation_8h.html":[1,0,0],
+"documentation_8h_source.html":[1,0,0],
+"files.html":[1,0],
+"index.html":[],
+"ksmap_8c.html":[1,0,1],
+"ksmap_8h.html":[1,0,2],
+"ksmap_8h_source.html":[1,0,2],
+"md_README.html":[0],
+"pages.html":[]
+};
