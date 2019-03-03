2c02000e587f3422e882634a28234728b2a47b08
Gabriel V <gabrielvalenzuela12@gmail.com>
2019-03-03 13:52:41 -0300

    Add documentation. Start to think the view to solve the problem

---
+++ Laboratory_1/documentation.h
@@ -0,0 +1,43 @@
+/**
+********************************************************************************
+*       	Laboratory number one: Cration of a ksmap software
+*
+*  @author: Valenzuela Gabriel Emanuel.
+*  @subject: Operating Systems I - 7422.
+*  @professor: CE Pablo, Martinez.
+*  @university: Universidad Nacional de Córdoba - Facultad de Ciencias 
+*  Exactas, Físicas y Naturales.					  
+*  @date: March 2019
+******************************************************************************
+*/
+
+ 
+/**
+*  This software takes a serie of arguments from the user and print the 
+*  selected information. Search on the \proc directory and get 
+*  informataion 
+*  about the system. The input parameters work of the following way:
+*
+*  -a 
+*	Print all information avaible by the software
+*
+*  -c
+*	Print only the information refers to the CPU. That is cantity of 
+*	time's use of CPU by users,system and idle process. Amoung of 
+*	context change. Start date of the system. Number of process 
+*	creates from the day 0
+*
+*  -k  
+*	Print the information refers to the kernel. Version kernel, model 
+*	and type of CPU, amoung of time from the day 0, amoung of systems 
+*	suppor by the kernel
+*
+*  -h
+*	Makes the information size readble for humans
+*
+*  -l [interval duration]
+*	Print the number of petitions maked to the disk. Amoung of memory 
+*	configurated on the hardware. Amoung of memory avaible and list 
+*	the mean of load of a minute.
+*/
+
